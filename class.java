17b5bfa8f6a43803e65c9bbed00d6f4d730e64c5
amitraut <amitram902@gmail.com>
2018-06-08 22:40:38 +0530

    add class.java.

---
+++ class.java
@@ -0,0 +1,9 @@
+class Amit 
+{
+  public static void main(String arg[])
+  {   
+     system.out.println("hellow world");
+      system.out.println(" im learning java package");
+      }
+       }
+
